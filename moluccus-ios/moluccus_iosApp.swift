ddb77c0f42071301392535a690b90ba307e53a7d
la niina <la.niina.me@gmail.com>
2023-04-07 10:20:54 +0300

    Initial Commit

---
+++ moluccus-ios/moluccus_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  moluccus_iosApp.swift
+//  moluccus-ios
+//
+//  Created by la niina on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct moluccus_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
